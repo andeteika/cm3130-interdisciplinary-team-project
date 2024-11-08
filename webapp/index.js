c5205f5fa8d0548cb3d1d09f785b4045329abe17
Adam Bisinella <110183985+andeteika@users.noreply.github.com>
2024-11-08 06:25:29 +0000

    Initial commit

---
+++ webapp/index.js
@@ -0,0 +1,25 @@
+// Import packages
+const express = require('express');
+
+
+// Express server
+const app = express();
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// TIP: Use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
